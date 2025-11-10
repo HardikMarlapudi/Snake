6554180161f2914b8e8e53ace5b20471d1f58145
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-11-09 22:15:45 -0500

    added the scoring system when the game over alert shows up

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function drawGame() {
     if (head.x < 0 || head.x >= canvas.width || head.y < 0 || head.y >= canvas.height) { 
             clearInterval(game);
             setTimeout(() => {
-            alert("Game over!");
+            alert(`Game Over! You scored ${score} points.`);
             startGame();
         }, 1000);
         return;
@@ -77,7 +77,7 @@ function drawGame() {
         if (head.x === segment.x && head.y === segment.y) {
             clearInterval(game);
             setTimeout(() => {
-                alert("Game over!");
+                alert(`Game Over! You scored ${score} points.`);
                 startGame();
             }, 1000);
             return;
